55cf8170a1d6341bead70c8af286b220dcbaff99
mrsarthakgupta <sarthakgupta007brh@gmail.com>
2025-10-16 13:47:17 +0530

    chnages

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
+  mongoose.connection.on('connected', () => {
     console.log("✅ Database Connected");
-  });
+  })
 
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
 };
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
-import useRouter from "./routes/userRoutes.js";
-import imageRouter from "./routes/imagesRoutes.js";
 
 // App Config
 const PORT = process.env.PORT || 4000;
@@ -16,7 +14,5 @@ app.use(cors());
 
 // API Routes
 app.get("/", (req, res) => res.send("API is working"));
-app.use("/api/user", useRouter);
-app.use("/api/image", imageRouter);
 
 app.listen(PORT, () => console.log("Server running on port: ", PORT));
